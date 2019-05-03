a9a880736f9b30fd3bd8627769d7f0aac0389efc
Filippo Pizzicola <fpizzicol@gmail.com>
2019-05-03 18:49:57 -0300

    Cria estrutura para ler os arquivos do firestore

--- xstudyout/src/main/kotlin/br/com/hacktur/xstudyout/controller/IdiomaController.kt
+++ xstudyout/src/main/kotlin/br/com/hacktur/xstudyout/controller/IdiomaController.kt
@@ -8,8 +8,9 @@ class IdiomaController {
 
     @GetMapping
     fun list(): List<Idioma> {
-        return listOf(Idioma("Inglês", "EUA"),
-                Idioma("Francês", "França"))
+        return listOf(
+                Idioma("Inglês", "https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg"),
+                Idioma("Francês", "https://upload.wikimedia.org/wikipedia/commons/6/62/Flag_of_France.png"))
     }
 
 }
--- xstudyout/src/main/kotlin/br/com/hacktur/xstudyout/model/Idioma.kt
+++ xstudyout/src/main/kotlin/br/com/hacktur/xstudyout/model/Idioma.kt
@@ -1,4 +1,29 @@
-data class Idioma(val nome: String, val bandeira: String) {
+import com.google.cloud.firestore.DocumentSnapshot
 
-    fun toMapFirebase(): Map<String, Any> = hashMapOf("nome" to nome, "bandeira" to bandeira)
+class Idioma {
+    var nome: String = ""
+    var bandeira: String = ""
+    var document: String = ""
+
+    constructor(nome: String, bandeira: String) {
+        this.nome = nome
+        this.bandeira = bandeira
+    }
+
+    constructor(dataDoc: DocumentSnapshot) {
+        this.document = dataDoc.id
+        this.nome = dataDoc.getString("nome") ?: ""
+        this.bandeira = dataDoc.getString("bandeira") ?: ""
+    }
+
+    fun toMapFirebase(): HashMap<String, String> {
+        val map = hashMapOf("nome" to nome,
+                "bandeira" to bandeira)
+
+        if (document.isNotBlank()) {
+            map["document"] = document
+        }
+
+        return map
+    }
 }

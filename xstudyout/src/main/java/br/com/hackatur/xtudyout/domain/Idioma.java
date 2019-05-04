1bb42f179f18f125ae34cf9e1132932212ce0f6d
Filippo Pizzicola <fpizzicol@gmail.com>
2019-05-03 22:05:11 -0300

    Criada rotina com o Groovy

--- xstudyout/src/main/java/br/com/hackatur/xtudyout/domain/Idioma.java
+++ xstudyout/src/main/java/br/com/hackatur/xtudyout/domain/Idioma.java
@@ -1,9 +1,15 @@
 package br.com.hackatur.xtudyout.domain;
 
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -11,7 +17,35 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Idioma {
 
+    public static String child = "idioma";
+
     private String nome;
     private String bandeira;
+    private String documento;
+
+    public Idioma(String nome, String bandeira) {
+        this.nome = nome;
+        this.bandeira = bandeira;
+    }
+
+    public Idioma(DocumentSnapshot doc) {
+        this.documento = doc.getId();
+        this.bandeira = doc.getString("bandeira");
+        this.nome = doc.getString("nome");
+    }
+
+    public Map<String, Object> toMap(){
+        Map<String, Object> resp = new HashMap<>();
+        resp.put("nome", getNome());
+        resp.put("bandeira", getBandeira());
+
+        if(StringUtils.isNotBlank(getDocumento())){
+            resp.put("documento", getDocumento());
+        }
+
+        return resp;
+    }
+
+
 
 }
--- xstudyout/src/main/java/br/com/hackatur/xtudyout/repository/IdiomaRepository.groovy
+++ xstudyout/src/main/java/br/com/hackatur/xtudyout/repository/IdiomaRepository.groovy
@@ -1,22 +1,45 @@
 package br.com.hackatur.xtudyout.repository;
 
-import br.com.hackatur.xtudyout.domain.Idioma;
+import br.com.hackatur.xtudyout.domain.Idioma
+import com.google.api.core.ApiFuture
+import com.google.cloud.firestore.DocumentReference
+import com.google.cloud.firestore.Firestore
+import com.google.cloud.firestore.QuerySnapshot
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.List
+import java.util.concurrent.ExecutionException;
 
 @Repository
 class IdiomaRepository {
 
+    @Autowired
+    Firestore firestore;
+
     Idioma save(Idioma idioma) {
-        new Idioma()
+        ApiFuture<DocumentReference> apiFuture = firestore.collection(Idioma.child).add(idioma.toMap())
+        try {
+            return new Idioma(apiFuture.get().get().get())
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace()
+        }
+        return null
     }
 
     List<Idioma> findAll() {
-        [
-            new Idioma("Inglês", "https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg"),
-            new Idioma("Francês", "https://upload.wikimedia.org/wikipedia/commons/6/62/Flag_of_France.png")
-        ]
+        save(new Idioma("Inglês", "https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg"))
+        save(new Idioma("Francês", "https://upload.wikimedia.org/wikipedia/commons/6/62/Flag_of_France.png"))
+
+        ApiFuture<QuerySnapshot> apiFuture = firestore.collection(Idioma.child).get()
+        List<Idioma> resp = new ArrayList<>()
+        try {
+            resp = apiFuture.get().getDocuments().collect { new Idioma(it) }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace()
+        }
+
+        return resp
     }
 }
